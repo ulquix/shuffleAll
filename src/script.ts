122366d66816a676917b7ea212584a8d30c9be99
Prince Yadav <princeyadav392005@gmail.com>
2025-12-06 20:29:01 +0530

    le bhai tu bhi hoja

---
+++ src/script.ts
@@ -0,0 +1,102 @@
+const clientId = "f5380c19e1374deaaa40f63bc4c49c56"; // Replace with your client id
+console.log("Client ID:", clientId);
+
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+export async function redirectToAuthCodeFlow(clientId: string) {
+    const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", "https://shuffle-all.vercel.app");
+    params.append("scope", "user-read-private user-read-email");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+function generateCodeVerifier(length: number) {
+    let text = '';
+    // eslint-disable-next-line prefer-const
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+async function generateCodeChallenge(codeVerifier: string) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function fetchProfile(token: string): Promise<any> {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    return await result.json();
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function populateUI(profile: any) {
+    document.getElementById("displayName")!.innerText = profile.display_name;
+    if (profile.images[0]) {
+        const profileImage = new Image(200, 200);
+        profileImage.src = profile.images[0].url;
+        document.getElementById("avatar")!.appendChild(profileImage);
+    }
+    document.getElementById("id")!.innerText = profile.id;
+    document.getElementById("email")!.innerText = profile.email;
+    document.getElementById("uri")!.innerText = profile.uri;
+    document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
+    document.getElementById("url")!.innerText = profile.href;
+    document.getElementById("url")!.setAttribute("href", profile.href);
+    document.getElementById("imgUrl")!.innerText = profile.images[0]?.url ?? '(no profile image)';
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function getAccessToken(clientId: string, code: string): Promise<string> {
+    if(!code) {
+        throw new Error("Authorization code is required");
+    }
+   const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", "https://shuffle-all.vercel.app");
+    params.append("scope", "user-read-private user-read-email");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params
+    });
+
+    const { access_token } = await result.json();
+    return access_token;
+}
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    populateUI(profile);
+}
