8fd916c494029554b69fd6dc06a399650dcfc214
Prince Yadav <princeyadav392005@gmail.com>
2025-12-08 20:32:39 +0530

    fas

--- src/TokenProvider.ts
+++ src/TokenProvider.ts
@@ -1,4 +1,4 @@
-import { fetchSavedAlbums, getAccessToken, redirectToAuthCodeFlow } from './script';
+import {  getAccessToken, redirectToAuthCodeFlow } from './script';
 
 const Authcomponent = async() => {
     const params = new URLSearchParams(window.location.search);
@@ -44,9 +44,7 @@ const code = params.get("code");
             }
             else{
               console.log("token valid, no action needed");
-              fetchSavedAlbums(token!).then(albums => {
-                console.log("User's Saved Albums:", albums);
-              });
+             
             }
         }
   
